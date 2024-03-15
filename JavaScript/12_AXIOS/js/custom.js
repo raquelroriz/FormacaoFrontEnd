5d6568e8a757b5ee1163ac8d60a33dc931713508
Raquel Roriz <maria.raquel.roriz@gmail.com>
2024-03-15 22:56:26 +0100

    29 utilizando AXIOS

---
+++ JavaScript/12_AXIOS/js/custom.js
@@ -0,0 +1,8 @@
+const postsFetch = axios.creat({
+  baseURL: "https://jsonplaceholder.typicode.com",
+  headers: {
+    Accept: "aplication/jason",
+    Authorization: "meunovotoken"
+  }
+})
+
---
+++ JavaScript/12_AXIOS/js/interceptors.js
@@ -0,0 +1,21 @@
+// Requisicao
+postsFetch.interceptors.request.use(
+  function(config) {
+    console.log("Antes da requisicao...");
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error)
+  }
+)
+
+// Resposta
+postsFetch.interceptors.response.use(
+  function(response) {
+    console.log("Antes da resposta...");
+    return response;
+  },
+  function(error) {
+    return Promise.reject(error)
+  }
+)
