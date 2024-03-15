ea46f320d68b3df2ce0f4a584f6e2d3545541dad
Raquel Roriz <maria.raquel.roriz@gmail.com>
2024-03-15 23:50:32 +0100

    30 projeto Lazy loading com JS

---
+++ JavaScript/LAZY_LOAD_JS/js/scripts.js
@@ -0,0 +1,18 @@
+// intersection observer API 
+// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+const images = document.querySelectorAll(".image-container img")
+const observer = new IntersectionObserver((entries, observer) => {
+
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) return 
+      const image = entry.target
+      image.src = image.src.replace("&w=10", "&w=1000")
+      observer.unobserve(image)
+    })
+
+}, {})
+
+images.forEach((image) => {
+  observer.observe(image)
+});
+
