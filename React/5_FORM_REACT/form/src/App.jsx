4331b80276acb7be83fc76c1e23b344ba090c439
Raquel Roriz <maria.raquel.roriz@gmail.com>
2024-07-11 20:21:53 +0200

    39 Css React

---
+++ React/5_FORM_REACT/form/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Myform from './components/Myform'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <h1>Form em React</h1>
+      <Myform userName="Raquel" userEmail="maria.raquel.roriz@gmail.com" />
+    </div>
+  )
+}
+
+export default App
